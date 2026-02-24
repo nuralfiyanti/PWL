e850dfc291dfb9ca464744c1beb0e91639b4fc9b
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-02-24 14:54:45 +0700

    Week 02 - Praktikm 1 menampilkan output Halaman Artikel dengan ID {id}

--- week-02/routes/web.php
+++ week-02/routes/web.php
@@ -36,3 +36,7 @@ Route::get('/user/{name}', function ($name) {
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
 return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
