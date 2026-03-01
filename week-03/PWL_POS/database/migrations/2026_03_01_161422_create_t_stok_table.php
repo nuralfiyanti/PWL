3c15a81cd798300e9597911756c876997a60dc22
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-03-01 23:30:57 +0700

    week-03 praktikum 2.2 - Migration semua tabel dengan foreign key

---
+++ week-03/PWL_POS/database/migrations/2026_03_01_161422_create_t_stok_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+            $table->id('stok_id');
+            $table->unsignedBigInteger('supplier_id');
+            $table->unsignedBigInteger('barang_id');
+            $table->integer('stok_jumlah');
+            $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('supplier_id')->references('supplier_id')->on('m_supplier');
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stok');
+    }
+};
