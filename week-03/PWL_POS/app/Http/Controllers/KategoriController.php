4092de462d6b04c96502151dfb44ca27b8d84c5b
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-03-02 03:02:26 +0700

    week-03 - praktikum 5 Implementasi  Query Builder view data tabel kategori

--- week-03/PWL_POS/app/Http/Controllers/KategoriController.php
+++ week-03/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -10,7 +10,7 @@ class KategoriController extends Controller
     public function index()
     {
         // INSERT data SNK
-      /*  $data = [
+       $data = [
             'kategori_kode' => 'SNK',
             'kategori_nama' => 'Snack/Makanan Ringan',
             'created_at' => now(),
@@ -19,18 +19,21 @@ class KategoriController extends Controller
         
         DB::table('m_kategori')->insert($data);
         
-        return 'Insert data SNK berhasil!'; */
+        return 'Insert data SNK berhasil!'; 
 
         // UPDATE data SNK
-        //$row = DB::table('m_kategori')
-        //  ->where('kategori_kode', 'SNK')
-         //   ->update(['kategori_nama' => 'Camilan']);
+        $row = DB::table('m_kategori')
+          ->where('kategori_kode', 'SNK')
+            ->update(['kategori_nama' => 'Camilan']);
     
-        //return 'Update data berhasil. Jumlah data diupdate: ' . $row . ' baris';
+        return 'Update data berhasil. Jumlah data diupdate: ' . $row . ' baris';
 
         $row = DB::table('m_kategori')
             ->where('kategori_kode', 'SNK')
             ->delete();
         return 'Delete data berhasil. Jumlah data dihapus: ' . $row . ' baris';
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
     }
 }
