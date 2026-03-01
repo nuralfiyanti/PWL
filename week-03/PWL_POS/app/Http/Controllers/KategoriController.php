7b51c251dc56de8fd21eb87a96246f3b842aeac7
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-03-02 02:32:14 +0700

    week-03 - praktikum 5 Implementasi  Query Builder 1 tambah data tabel kategori

---
+++ week-03/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index()
+    {
+        // INSERT data SNK
+      /*  $data = [
+            'kategori_kode' => 'SNK',
+            'kategori_nama' => 'Snack/Makanan Ringan',
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+        
+        DB::table('m_kategori')->insert($data);
+        
+        return 'Insert data SNK berhasil!'; */
+
+        
+    }
+}
