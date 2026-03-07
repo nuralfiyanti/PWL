18f1c3ce1ee4e5b08979cd4cfd4f9887ce5e117d
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-03-07 18:13:25 +0700

    week-04 - praktikum 2.1 retrieving single models bagian 1 (find)

--- week-04/PWL_POS/app/Http/Controllers/UserController.php
+++ week-04/PWL_POS/app/Http/Controllers/UserController.php
@@ -4,24 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        // Ambil semua data dari tabel m_user
-        $user = UserModel::all();
-        
-        // Kirim data ke view
+        $user = UserModel::find(1);
         return view('user', ['data' => $user]);
     }
 }
