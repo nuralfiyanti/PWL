3783e5b08aadeaf5880d913e31825acbce8bee17
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-03-08 07:51:38 +0700

    week-04 - praktikum 2.5  Attribute Changes (wasChanged)

--- week-04/PWL_POS/app/Http/Controllers/UserController.php
+++ week-04/PWL_POS/app/Http/Controllers/UserController.php
@@ -8,42 +8,28 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        // Buat user baru dengan username unik
-        $user = UserModel::create([
-            'username' => 'manager55_' . rand(100, 999), 
-            'nama' => 'Manager55',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-        
-        echo "User baru: " . $user->username . "<br>";
-        
-        // Ubah username
-        $user->username = 'manager56_' . rand(100, 999); 
-        
-        echo "Setelah diubah: " . $user->username . "<br><br>";
-        
-        // Cek isDirty
-        echo "isDirty(): " . ($user->isDirty() ? 'true' : 'false') . "<br>";
-        echo "isDirty('username'): " . ($user->isDirty('username') ? 'true' : 'false') . "<br>";
-        echo "isDirty('nama'): " . ($user->isDirty('nama') ? 'true' : 'false') . "<br>";
-        echo "isDirty(['nama', 'username']): " . ($user->isDirty(['nama', 'username']) ? 'true' : 'false') . "<br><br>";
-        
-        // Cek isClean
-        echo "isClean(): " . ($user->isClean() ? 'true' : 'false') . "<br>";
-        echo "isClean('username'): " . ($user->isClean('username') ? 'true' : 'false') . "<br>";
-        echo "isClean('nama'): " . ($user->isClean('nama') ? 'true' : 'false') . "<br>";
-        echo "isClean(['nama', 'username']): " . ($user->isClean(['nama', 'username']) ? 'true' : 'false') . "<br><br>";
-        
-        // Simpan perubahan
-        $user->save();
-        
-        echo "Setelah save:<br>";
-        echo "isDirty(): " . ($user->isDirty() ? 'true' : 'false') . "<br>";
-        echo "isClean(): " . ($user->isClean() ? 'true' : 'false') . "<br>";
-        
-        return;
-    }
+public function index()
+{
+    $user = UserModel::create([
+        'username' => 'manager11_' . rand(100, 999),
+        'nama' => 'Manager11',
+        'password' => Hash::make('12345'),
+        'level_id' => 2,
+    ]);
+    
+    echo "User baru: " . $user->username . "<br>";
+    
+    $user->username = 'manager12_' . rand(100, 999);
+    echo "Setelah diubah: " . $user->username . "<br><br>";
+    
+    $user->save();
+    
+    echo "wasChanged(): " . ($user->wasChanged() ? 'true' : 'false') . "<br>";
+    echo "wasChanged('username'): " . ($user->wasChanged('username') ? 'true' : 'false') . "<br>";
+    echo "wasChanged(['username', 'level_id']): " . ($user->wasChanged(['username', 'level_id']) ? 'true' : 'false') . "<br>";
+    echo "wasChanged('nama'): " . ($user->wasChanged('nama') ? 'true' : 'false') . "<br>";
+    echo "wasChanged(['nama', 'username']): " . ($user->wasChanged(['nama', 'username']) ? 'true' : 'false') . "<br>";
+    
+    return;
+}
 }
