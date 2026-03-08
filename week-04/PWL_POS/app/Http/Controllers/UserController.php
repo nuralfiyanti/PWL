a28a13c2e300717c2302cf704698039bf8d1b5c9
nuralfiyanti <nuralfiyanti05@gmail.com>
2026-03-08 08:15:10 +0700

    week-04 - praktikum 2.6  CRUD (bagian 1)

--- week-04/PWL_POS/app/Http/Controllers/UserController.php
+++ week-04/PWL_POS/app/Http/Controllers/UserController.php
@@ -8,28 +8,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-public function index()
-{
-    $user = UserModel::create([
-        'username' => 'manager11_' . rand(100, 999),
-        'nama' => 'Manager11',
-        'password' => Hash::make('12345'),
-        'level_id' => 2,
-    ]);
-    
-    echo "User baru: " . $user->username . "<br>";
-    
-    $user->username = 'manager12_' . rand(100, 999);
-    echo "Setelah diubah: " . $user->username . "<br><br>";
-    
-    $user->save();
-    
-    echo "wasChanged(): " . ($user->wasChanged() ? 'true' : 'false') . "<br>";
-    echo "wasChanged('username'): " . ($user->wasChanged('username') ? 'true' : 'false') . "<br>";
-    echo "wasChanged(['username', 'level_id']): " . ($user->wasChanged(['username', 'level_id']) ? 'true' : 'false') . "<br>";
-    echo "wasChanged('nama'): " . ($user->wasChanged('nama') ? 'true' : 'false') . "<br>";
-    echo "wasChanged(['nama', 'username']): " . ($user->wasChanged(['nama', 'username']) ? 'true' : 'false') . "<br>";
-    
-    return;
-}
+    public function index()
+    {
+    $user = UserModel::all();
+    return view('user', ['data' => $user]);
+    }
 }
